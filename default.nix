6c6fbd45bbd4b514c6d586ff271e6404eb25340a
Maarten Hoogendoorn <maarten@moretea.nl>
2016-12-23 11:46:28 +0100

    Generate container that runs all tests in tests/ dir

--- default.nix
+++ default.nix
@@ -1,3 +1,25 @@
 { nixpkgs ? (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/1dd0fb6b5a7c44d1b632466f936ca74268d13298.tar.gz") }:
 with import nixpkgs {}; 
-callPackage ./runner {}
+rec {
+  runner = callPackage ./runner {};
+  runner-image = dockerTools.buildImage {
+    name = "weaveworksdemos/e2etests-runner";
+    contents = runner;
+    config = {
+      Entrypoint = [ "${runner}/bin/e2etest"];
+    };
+  };
+
+  e2etest-image = let
+    tests = ./tests;
+  in
+    dockerTools.buildImage {
+    name = "weaveworksdemos/e2etests";
+    contents = runner;
+    config = {
+      WorkingDir = "${tests}";
+      Entrypoint = [ "${runner}/bin/e2etest"];
+      Cmd = with builtins; attrNames (readDir ./tests);
+    };
+  };
+}
--- runner/default.nix
+++ runner/default.nix
@@ -1,4 +1,4 @@
-{ ruby, bundlerEnv, stdenv, runCommand, phantomjs }:
+{ bash, ruby, bundlerEnv, phantomjs, writeScriptBin }:
 let
   env = bundlerEnv {
     name = "e2e-ruby-env";
@@ -8,23 +8,11 @@ let
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
   };
-
-  runner = stdenv.mkDerivation {
-    name = "e2etest";
-    phases = "installPhase";
-    installPhase = ''
-      mkdir -p $out/bin
-      echo "export PATH=\$PATH:${phantomjs}/bin; ${env}/bin/bundle exec ${./bin/e2etest} \$@" > $out/bin/e2etest;
-      chmod +x $out/bin/e2etest
-    '';
-  };
-
-  mkReport = file: 
-   runCommand 
-     "make-e2e-report" 
-     { }
-     "${runner}/bin/e2etest ${file} 2>&1 > $out";
-in {
-  inherit runner;
-  inherit mkReport;
-}
+in
+writeScriptBin
+  "e2etest"
+  ''
+  #!/${bash}/bin/bash
+  export PATH=$PATH:${phantomjs}/bin
+  ${env}/bin/bundle exec ${./bin/e2etest} $@
+  ''
